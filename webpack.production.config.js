8cd019922fd1ac3d8479b4dc33378d191809e3e9
zhiruoyu <m_convert@163.com>
2017-08-16 18:09:16 +0800

    change

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,14 +1,15 @@
 var path = require('path');
+var ROOTPATH = path.resolve(__dirname,'./');
+console.log(ROOTPATH)
 var OpenBrowserPlugin  = require('open-browser-webpack-plugin');
-var htmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
 module.exports = {
-    devtool:'eval-source-map',
+    devtool:'cheap-eval-source-map',
     entry:'./app/main.js',
     output:{
-        path:path.resolve(__dirname,'./dist'),
-        filename:'[name].js',
-        // publicPath:'./dist'
+        path:ROOTPATH+'/dist',
+        filename:'bundle.js',
+        publicPath:'./dist'
     },
     module:{
         loaders:[
@@ -19,7 +20,7 @@ module.exports = {
         },
         {
             test:/\.css$/,
-            loader:'style-loader!css-loader!postcss-loader'
+            loader:'style-loader!css-loader!postcss-loader!'
         },{
             test:/\.less$/,
             loader:'style-loader!css-loader!postcss-loader!less-loader'
@@ -27,20 +28,11 @@ module.exports = {
         ]
     },
     plugins:[
-    new webpack.HotModuleReplacementPlugin(),
-    new htmlWebpackPlugin({
-        title:'hi',
-        template:'./index.html',
-        inject:true,
-        hash:true,
-        exclude:/node_modules/
-    }),
+    // new HotModuleReplacementPlugin(),
     new OpenBrowserPlugin({url:'http://localhost:8080'})
     ],
     devServer:{
-        contentBase:'./dist',
         historyApiFallback:true,
         inline:true,
-        hot:true,
     }
 }
